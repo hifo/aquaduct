5b8f4889326145007ae8affd5fb6862b322d47fc
Hillary <hifo@wpi.edu>
2011-05-05 22:26:24 -0400

    added background to game. Unsure if the scale is good or not

--- game.js
+++ game.js
@@ -142,8 +142,7 @@ function draw () {
 
     ctx.save ();
 
-    ctx.fillStyle = "rgb(0, 0, 0)";
-    ctx.fillRect (0, 0, canvas.width, canvas.height);
+    safe_draw_image(ctx, background, 0,0, GRID_SIZE*24, GRID_SIZE*12);
 
     ctx.restore ();
 
@@ -235,6 +234,8 @@ function init () {
 
     cursor_aqueduct = new Aqueduct (0, 0, "right");
     goal_city = load_image("goal_city.png");
+    
+    background = load_image("background.png");
 
     $(canvas).mousedown (mouse_down);
     $(canvas).mousemove (mouse_motion);
